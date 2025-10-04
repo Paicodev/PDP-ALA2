01b0b1365ff8ac23a6a64b9cafe06d9ac658d4c4
Pablo Aguilar <paedmaag@gmail.com>
2025-10-04 17:01:23 -0300

    Update a Editar.ts
    -Se convirtió el CommonJS  a ES6 en la forma de importar y exportar.
    -Se agregaron tipados a funciones y variables
    -Los inputs se manejan limpiando espacios y cambiando a mayusculas con toUpperCase.
    -Manejo correcto de fechas: esta parte es la que mas me costó, ahora en vez de hacer un tolocalestring(). Defino a Vencimiento en Tarea como Date | null.
    Esto luego me permite guardar un null si no se agrega fecha de vencimiento en vez de "", que seria tipo string. Si se ingresa algo, se trata de convertir a Date con la validacion isNan(fecha.getTime())
    
    La fecha de edicion se guardaba con toLocaleString(). Ahora se guarda como Date directamente. coincidiendo con la interfaz Tarea en Data.ts

--- src/editar.ts
+++ src/editar.ts
@@ -1,8 +1,8 @@
-// editar.js
-const { tareas } = require("./data");
-const { input } = require("./entradas");
+// editar.ts
+import { tareas, Tarea } from "./data";
+import { input } from "./entradas";
 
-async function editarTarea(indice) {
+async function editarTarea(indice: number): Promise<void> {
   const tarea = tareas[indice];
 
   console.log(`\nEstas editando la tarea "${tarea.titulo}".`);
@@ -20,33 +20,38 @@ async function editarTarea(indice) {
   let nuevoEstado = await input(
     `2. Estado ([P]endiente / [E]n curso / [T]erminada / [C]ancelada) [${tarea.estado}]: `
   );
-  if (nuevoEstado === " ") tarea.estado = "";
-  else if (nuevoEstado !== "") {
-    if (nuevoEstado.toUpperCase() === "P") tarea.estado = "Pendiente";
-    else if (nuevoEstado.toUpperCase() === "E") tarea.estado = "En Curso";
-    else if (nuevoEstado.toUpperCase() === "T") tarea.estado = "Terminada";
-    else if (nuevoEstado.toUpperCase() === "C") tarea.estado = "Cancelada";
+  if (nuevoEstado.trim() !== ""){
+    const est = nuevoEstado.trim().toUpperCase();
+    if (est === "P") tarea.estado = "Pendiente";
+    else if (est === "E") tarea.estado = "En Curso";
+    else if (est === "T") tarea.estado = "Terminada";
+    else if (est === "C") tarea.estado = "Cancelada";
     else console.log("Valor inválido, se mantiene el estado anterior.");
   }
 
   // 3. Dificultad
   let nuevaDif = await input(`3. Dificultad ([1] / [2] / [3]) [${tarea.dificultad}]: `);
-  if (nuevaDif === " ") tarea.dificultad = "";
-  else if (nuevaDif !== "") {
-    if (["1", "2", "3"].includes(nuevaDif)) tarea.dificultad = nuevaDif;
+  if (nuevaDif.trim() !== ""){
+    const difNum = parseInt(nuevaDif);
+    if ([1, 2, 3].includes(difNum)) tarea.dificultad = difNum;
     else console.log("Valor inválido, se mantiene la dificultad anterior.");
   }
 
   // 4. Vencimiento
   let nuevoVenc = await input(`4. Vencimiento [${tarea.vencimiento}]: `);
-  if (nuevoVenc === " ") tarea.vencimiento = "";
-  else if (nuevoVenc !== "") tarea.vencimiento = nuevoVenc;
+
+  if (nuevoVenc === " ") tarea.vencimiento = null;
+  else if (nuevoVenc.trim() !== ""){
+    const fecha = new Date(nuevoVenc);
+    if (!isNaN(fecha.getTime())) tarea.vencimiento =fecha;
+    else console.log("Fecha inválida, se mantiene la fecha anterior.");
+  }
 
   // BONUS: actualizar "última edición"
-  tarea.edicion = new Date().toLocaleString();
+  tarea.edicion = new Date();
 
   console.log("\n¡Datos guardados!");
   await input("Presiona cualquier tecla para continuar…");
 }
 
-module.exports = { editarTarea };
+export { editarTarea };
